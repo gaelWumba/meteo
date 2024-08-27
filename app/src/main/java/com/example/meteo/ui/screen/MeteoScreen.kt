179feb9aa2bbc243d52230c1bdfd7ca90ac805a0
gaelWumba <gael.wumbabuatu@gmail.com>
2024-08-27 04:42:58 +0200

    Top action bar

---
+++ app/src/main/java/com/example/meteo/ui/screen/MeteoScreen.kt
@@ -0,0 +1,31 @@
+package com.example.meteo.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.meteo.ui.screen.components.ActionBar
+import com.example.meteo.ui.theme.ColorBackground
+
+@Composable
+fun MeteoScreen(){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = ColorBackground
+    ) { paddings ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddings)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 10.dp
+                )
+        ) {
+            ActionBar()
+        }
+    }
+}
