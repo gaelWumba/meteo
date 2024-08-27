93bfcf32f35b815fe47621080b2f72a92ff2ee56
gaelWumba <gael.wumbabuatu@gmail.com>
2024-08-27 19:53:29 +0200

    Daily forecast UI

--- app/src/main/java/com/example/meteo/ui/screen/MeteoScreen.kt
+++ app/src/main/java/com/example/meteo/ui/screen/MeteoScreen.kt
@@ -1,13 +1,16 @@
 package com.example.meteo.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.meteo.ui.screen.components.ActionBar
+import com.example.meteo.ui.screen.components.DailyForecast
 import com.example.meteo.ui.theme.ColorBackground
 
 @Composable
@@ -26,6 +29,10 @@ fun MeteoScreen(){
                 )
         ) {
             ActionBar()
+            Spacer(
+                modifier = Modifier.height(12.dp)
+            )
+            DailyForecast()
         }
     }
 }
